96136f63925279654f3ef50c951232f74a8ffc1a
茜雫 <lartimes2004@163.com>
2025-05-13 20:23:12 +0800

    推一下代码

--- spring-ai-alibaba-demo/src/main/java/bankai/shizuku/ichigo/SpringAiAlibabaDemoApplication.java
+++ spring-ai-alibaba-demo/src/main/java/bankai/shizuku/ichigo/SpringAiAlibabaDemoApplication.java
@@ -1,13 +1,36 @@
 package bankai.shizuku.ichigo;
 
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Map;
 
 @SpringBootApplication
-public class SpringAiAlibabaDemoApplication {
+public class SpringAiAlibabaDemoApplication implements CommandLineRunner {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     public static void main(String[] args) {
         SpringApplication.run(SpringAiAlibabaDemoApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        Map<String, ChatModel> map = applicationContext.getBeansOfType(ChatModel.class);
+        System.out.println("===============================chatModel");
+        map.forEach((k, v) -> {
+            System.out.println(k + " " + v);
+        });
+//        Map<String, ChatClient> map1 = applicationContext.getBeansOfType(ChatClient.class);
+//        System.out.println("===============================chatClient");
+//        map1.forEach((k, v) -> {
+//            System.out.println(k + " " + v);
+//        });
+
+    }
 }
--- spring-ai-alibaba-demo/src/main/java/bankai/shizuku/ichigo/controller/ChatController.java
+++ spring-ai-alibaba-demo/src/main/java/bankai/shizuku/ichigo/controller/ChatController.java
@@ -3,11 +3,11 @@ package bankai.shizuku.ichigo.controller;
 import jakarta.annotation.Resource;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
-import org.springframework.ai.chat.model.Generation;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class ChatController {
@@ -15,12 +15,17 @@ public class ChatController {
     @Resource(name = "openAiChatModel")
     private ChatModel deepSeekChatModel;
 
+    @Resource(name = "dashscopeChatModel")
+    private ChatModel chatModel;
+
+
+
 
     @GetMapping("/{prompt}")
-    public Generation chat(@PathVariable(value = "prompt") String prompt) {
-
-        ChatResponse chatResponse = deepSeekChatModel.call(new Prompt(prompt));
-        return chatResponse.getResult();
+    public String chat(@PathVariable(value = "prompt") String prompt) {
+        Flux<ChatResponse> stream = chatModel.stream(new Prompt(prompt + "不要输出思考过程，只需要给出结果：" + prompt));
+        stream.subscribe(System.out::println);
+        return prompt;
     }
 
 }
